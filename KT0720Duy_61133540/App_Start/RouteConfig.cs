4c52ad69a66ab053a97afcbf8027aaf0929bd7fb
Your Name <duy01657853612@gmail.com>
2022-08-10 22:55:39 +0700

    Bài tập MVC

--- KT0720Duy_61133540/App_Start/RouteConfig.cs
+++ KT0720Duy_61133540/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KT0720Duy_61133540
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GioiThieu_61133540", action = "GioiThieu_61133540", id = UrlParameter.Optional }
             );
         }
     }
